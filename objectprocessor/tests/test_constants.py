2bd221ed5d9e3a669c058d5453201b3033575a03
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-02-07 10:02:10 -0500

    test: Add a new test and improve documentation in the objectprocessor/tests/test_constants.py file.

--- objectprocessor/tests/test_constants.py
+++ objectprocessor/tests/test_constants.py
@@ -11,13 +11,26 @@
 # - All of the methods associated with the Person class.
 # - All of the data input and storage functions in process module.
 # - All of the data searching functions in the process module.
-# - All additional modules inside of the objectprocessor package.
+#
+# TODO: Please use the other provided test files to add
+# your test cases for all of the aforementioned items.
 
 from objectprocessor import constants
 
 
 def test_constant_values_for_person_attributes():
     """Ensure that all of the person_attributes constants have a value."""
+    # confirm that there are the correct number of constants
     assert len(vars(constants.person_attributes).items()) == 5
+    # confirm that all of the constants have a value
+    for _, value in vars(constants.person_attributes).items():
+        assert value is not None
+
+
+def test_constant_values_for_person_index():
+    """Ensure that all of the person_index constants have a value."""
+    # confirm that there are the correct number of constants
+    assert len(vars(constants.person_index).items()) == 5
+    # confirm that all of the constants have a value
     for _, value in vars(constants.person_attributes).items():
         assert value is not None
