62a894461c243f050bdca55b5df3aceeee7ae674
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-02-07 10:06:22 -0500

    test: Add the template file for the objectprocessor/tests/test_process.py.

---
+++ objectprocessor/tests/test_process.py
@@ -0,0 +1,9 @@
+"""Test suite for the process module."""
+
+# TODO: Add all of the required test cases for the process module
+
+# TODO: Note that some of the test cases in this test suite may require you to
+# explore the use of mock objects or pytest test fixtures. After investigating
+# these in greater detail and using generative AI techniques to help you to
+# create a starting point for your tests, you can talk with the course
+# instructor to learn more about how to add these components to your tests.
